67150a3b4827bc687bdd6a84c3f1fc489468a72b
Iven S <47639297+LvckyAPI@users.noreply.github.com>
2022-07-24 17:29:03 +0200

    Update RELEASE 2.0

---
+++ src/backend/sysadmin.ts
@@ -0,0 +1,54 @@
+import { bot } from '../index';
+import * as api from 'lvckyworld-api';
+import * as fs from 'fs';
+
+export async function handleMessages() {
+    bot.on('messageCreate', async (message) => {
+
+        if (!await api.MARINA.isSystemAdmin(message.author.id)) {
+            return;
+        }
+
+        var cmdPrefix = 'x!';
+        var command = message.content.toLowerCase().slice(cmdPrefix.length).split(" ")[0];
+        var args = message.content.slice(cmdPrefix.length).split(" ").slice(1);
+
+        switch (command) {
+            case 'leave':
+                await message.reply('Meister, Ihr Wunsch ist mir Befehl!\n`leave`');
+                await message.guild?.leave();
+                break;
+            case 'permleave':
+                var joinBlacklist = fs.readFileSync(
+                    __dirname + '/../data/joinBlacklist.json',
+                    { encoding: 'utf-8' })
+
+                interface blacklist {
+                    blacklist: [
+                        { guildId: string }
+                    ]
+                }
+
+                if (!message.guildId) return;
+                let list: blacklist = JSON.parse(joinBlacklist);
+                if (list.blacklist.some(elm => elm.guildId == message.guildId)) return;
+
+                const json = {
+                    guildId: message.guildId
+                }
+
+                list.blacklist.push(json);
+                fs.writeFileSync(
+                    __dirname + '/../data/joinBlacklist.json',
+                    JSON.stringify(list),
+                    { encoding: 'utf-8' }
+                )
+
+                await message.reply('Meister, Ihr Wunsch ist mir Befehl!\n`permleave`');
+                await message.guild?.leave();
+
+                break;
+        }
+
+    })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as Discord from 'discord.js';
+import * as fs from 'fs';
 import 'dotenv/config';
 
 export const bot = new Discord.Client({
@@ -24,6 +25,7 @@ export const bot = new Discord.Client({
 // import modules
 import * as joinHandler from './backend/joinHandler';
 import * as messageHandler from './backend/messageHandler';
+import * as sysadmin from './backend/sysadmin';
 
 
 
@@ -32,10 +34,11 @@ bot.on('ready', () => {
     joinHandler.checkNewUser();
     messageHandler.checkMessageAuthor();
     messageHandler.updateBanList();
+    sysadmin.handleMessages();
 
 
     // output Infos
-    console.info(`The bot is on ${bot.guilds.cache.size}`);
+    console.info(`The bot is on ${bot.guilds.cache.size} guilds.`);
     console.info(`The bot is started succsessfully.`);
     console.info(`The bot is logged in as ${bot.user?.username}#${bot.user?.discriminator}`);
 });
@@ -45,7 +48,23 @@ export class INSTANCE {
     public static apiPassword: string | undefined = process.env.API_PASSWORD?.replace("[HASHTAG]", "#");
 }
 
+bot.on('guildCreate', guild => {
+    var joinBlacklist = fs.readFileSync(
+        __dirname + '/data/joinBlacklist.json',
+        { encoding: 'utf-8' })
+
+    interface blacklist {
+        blacklist: [
+            { guildId: string }
+        ]
+    }
+
+    let list: blacklist = JSON.parse(joinBlacklist);
+    if (list.blacklist.some(elm => elm.guildId == guild.id)) guild.leave();
+})
+
 bot.on('error', (err) => {
     console.error(err);
 });
+
 bot.login(process.env.BOT_TOKEN);
